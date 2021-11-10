6adb3b0a4dd56a39359769868603960c8fc59b96
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-11 01:43:24 +0530

    Added: Event binding to open bridgelabz page in new tab on logo click

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import './App.css';
 import logo from './assets/img/BridgeLabzLogo.png'
 
 class App extends React.Component {
+
+  url = "https://www.bridgelabz.com/"
   constructor() {
     super();
     this.state = { title: "Hello From Bridgelabz!!!" }
+  }
+
+  OnClick = ($event) => {
+
+    window.open(this.url, "_blank");
   }
 
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src = {logo} alt = "Bridgelabz logo"></img>
+        <img src = {logo} onClick = { this.OnClick } alt = "Bridgelabz logo"></img>
       </div>
     );
   }

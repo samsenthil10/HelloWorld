717a02090029468cc9e1ae48a3cd0452090f182b
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-11 02:49:08 +0530

    Added: To fill the content of the page

--- src/App.js
+++ src/App.js
@@ -29,15 +29,40 @@ class App extends React.Component {
 
   render() {
     return (
-      <><div>
+      <><div class="placeAtCenter">
         <h1>Hello {this.state.userName} From Bridgelabz!!!</h1>
         <img src={logo} onClick={this.onClick} alt="Bridgelabz logo"></img>
-      </div><div>
+      </div><div class="placeAtCenter">
           <input onChange={this.onNameChange}></input>
           <span className="error-output">{this.state.nameError}</span>
-        </div></>
+        </div><main class="content">
+          <div id="article-content">
+            <article>
+              <p>At Bridgelabz,we're a techie community of</p>
+            </article>
+            <article>
+              <ul>
+                <li>Technologists</li>
+                <li>Thinkers</li>
+                <li>Builders</li>
+              </ul>
+              <p>
+                Working togeter to keep the tech Employability of engineers alive and accesible,so Tech Companies worldwide can get contributors and creators for Technology solutions.We believe this act of human collaboration across an Employability platform is essential
+                to individual growth and our collective future.
+              </p>
+            </article>
+            <article>
+              <p>
+                To know about us,visit <a href="https://www.bridgelabz.com/">Bridgelabz</a> to learn even more about our mission i.e <strong> Employability to all</strong>
+              </p>
+            </article>
+          </div>
+
+        </main></>
+
     );
   }
 }
 
 export default App;
+

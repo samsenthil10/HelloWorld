470464af0e4f1b4870c5a06d8f92b0416e282431
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-11 07:41:47 +0530

    Added: To set Route to display home component

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+const routing = (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/home" element={<App />} />
+      <Route path="home" element={<Home />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(
+  routing,
+  document.getElementById("root")
+);
+
 reportWebVitals();
